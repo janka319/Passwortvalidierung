6171b987e8b51fce813fe8d7a8c3b3e163f44385
felix-mohrmann <mohrmann.felix@gmail.com>
2021-07-16 12:29:35 +0200

    finished validation method for array

--- src/main/java/PasswordValidator.java
+++ src/main/java/PasswordValidator.java
@@ -1,31 +1,35 @@
 public class PasswordValidator {
     public static void main(String[] args) {
-        String[] passwords = {"asda1", "Pw1234", "CoseCopter2", "Password", "password", "Password1"};
-        for(int i=0; i<passwords.length; i++) {
-
+        String[] passwords = {"asda1", "Pw1234", "CaseCopter2", "Password", "password", "Password1"};
+        boolean valid;
+        for (int i = 0; i < passwords.length; i++) {
+            System.out.print("Password: "+ (i+1) + " (" + passwords[i] + ")");
+            valid = validation(passwords[i]);
+            System.out.println();
         }
     }
 
-    public static boolean validation (String password) {
-        boolean validate=true;
-        if(!hasLength(password)){
-            System.out.println("Your password is either too long or too short!");
-            validate=false;
+    public static boolean validation(String password) {
+        boolean validate = true;
+        if (!hasLength(password)) {
+            System.out.println("is either too long or too short!");
+            validate = false;
         }
-        if(!containsDigit(password)){
-            System.out.println("Your Password doesn´t contain a digit");
-            validate=false;
+        if (!containsDigit(password)) {
+            System.out.println("doesn't contain a digit");
+            validate = false;
         }
-        if(!containsLowerCase(password)){
-            System.out.println("Your password doesn´t contain a lower case letter");
-            validate=false;
+        if (!containsLowerCase(password)) {
+            System.out.println("doesn't contain a lower case letter");
+            validate = false;
         }
-        if(!containsUpperCase(password)){
-            System.out.println("Your password doesn´t contain an upper case letter");
-            validate=false;
+        if (!containsUpperCase(password)) {
+            System.out.println("doesn't contain an upper case letter");
+            validate = false;
         }
         return validate;
     }
+
     public static boolean hasLength(String password) {
         if (password.equals(null)) {
             return false;
@@ -38,9 +42,9 @@ public class PasswordValidator {
 
     public static boolean containsDigit(String password) {
         char c;
-        for (int i=0;i<password.length();i++){
+        for (int i = 0; i < password.length(); i++) {
             c = password.charAt(i);
-            if (Character.isDigit(c)){
+            if (Character.isDigit(c)) {
                 return true;
             }
         }
@@ -49,9 +53,9 @@ public class PasswordValidator {
 
     public static boolean containsLowerCase(String password) {
         char c;
-        for (int i=0;i<password.length();i++){
+        for (int i = 0; i < password.length(); i++) {
             c = password.charAt(i);
-            if (Character.isLowerCase(c)){
+            if (Character.isLowerCase(c)) {
                 return true;
             }
         }
@@ -61,9 +65,9 @@ public class PasswordValidator {
 
     public static boolean containsUpperCase(String password) {
         char c;
-        for (int i=0;i<password.length();i++){
+        for (int i = 0; i < password.length(); i++) {
             c = password.charAt(i);
-            if (Character.isUpperCase(c)){
+            if (Character.isUpperCase(c)) {
                 return true;
             }
         }
